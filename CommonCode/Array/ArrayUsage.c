c1e970f691943bf14a2abb0290feacd15cf38c78
cbolong <cbolong@gmail.com>
2017-02-27 22:01:25 +0800

    [CommonCode] Upload Array point pass to function
    Upload code

---
+++ CommonCode/Array/ArrayUsage.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void Multiply(int Array[4][4])
+{
+    int i, j;
+    
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            Array[i][j] = i*j;
+        }
+    }
+}
+
+void MultiplyPointer(int *Array)
+{
+    int i, j;
+    
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            *((Array+i*4)+j) = i*j;
+        }
+    }
+}
+
+void Println(int Array[4][4])
+{
+    int i, j;
+    
+    for(i=0; i<4; i++)
+    {
+        for(j=0; j<4; j++)
+        {
+            printf("[%d] ",Array[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void main()
+{
+    int Array_1[4][4];
+    
+    //Multiply(Array_1);
+    MultiplyPointer(&Array_1[0][0]);
+    Println(Array_1);
+}
