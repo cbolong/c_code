82ab426a6239de52b8f49b5b23a4d8f7eb488cf3
cbolong <cbolong@gmail.com>
2017-03-01 20:15:29 +0800

    [CommonCode] Eat space in source code

--- CommonCode/Array/CharToStruct.c
+++ CommonCode/Array/CharToStruct.c
@@ -11,13 +11,13 @@ static int SIZE;
 void Println(char buf[])
 {
     int i;
-	struct USB *temp;
+    struct USB *temp;
 	
-	temp = (struct USB *)buf;
+    temp = (struct USB *)buf;
 	
     printf("Char array - usb1:[%d] usb2:[%d] usb3:[%d]\n", temp->usb1, 
-													temp->usb2,
-													temp->usb3);
+						           temp->usb2,
+							   temp->usb3);
 
 }
 
@@ -26,13 +26,13 @@ int main()
     struct USB *mobile;
     char buf[SIZE];
 	
-	SIZE = sizeof(struct USB);
+    SIZE = sizeof(struct USB);
 	
-	memset(buf, 0, SIZE);
-	mobile = (struct USB *)buf;
-	mobile->usb1 = 7;
-	mobile->usb2 = 8;
-	mobile->usb3 = 9;
+    memset(buf, 0, SIZE);
+    mobile = (struct USB *)buf;
+    mobile->usb1 = 7;
+    mobile->usb2 = 8;
+    mobile->usb3 = 9;
 	
     Println(buf);
 	
