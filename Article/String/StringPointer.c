4799183379b6e3f02ced8ebd2f3dc3a0234d86c4
Vodka Chen <Bo-Rong_Chen@Alphanetworks.com>
2017-03-20 19:39:41 +0800

    [Article] Return array/pointer from function
    update code

---
+++ Article/String/StringPointer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* func() {
+	//char a[] = "Hello";	
+	//char* a = "Hello";
+	
+	/*Solution*/
+	//char *a;
+	//a = (char *)malloc(sizeof(char)*10);
+	//strcpy(a, "Hello");
+	
+	return a;
+}
+
+int main(void)
+{
+    char* s = func();
+	
+    printf("[%s]\n", s);
+    return 0;
+}
